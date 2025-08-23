e84ce808e553165140dd75fef8ceda2a86aed74b
Diya Gupta <192004gupta@gmail.com>
2025-08-23 22:45:08 +0530

    logging n exception

---
+++ networksecurity/exception/exception.py
@@ -0,0 +1,15 @@
+import sys 
+from networksecurity.logging import logger 
+
+class NetworkSecurityException(Exception):
+    def __init__(self,error_message,error_details:sys):
+        _,_,exc_tb=error_details.exc_info()
+        self.error_message=error_message
+        self.lineno=exc_tb.tb_lineno
+        self.file_name=exc_tb.tb_frame.f_code.co_filename 
+
+    def __str__(self):
+        return "Error occured in pythin script name[{0}] line number [{1}] error mssg [{2}]".format(
+            self.file_name,self.lineno,str(self.error_message)
+        )
+    
